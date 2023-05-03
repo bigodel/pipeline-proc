485b8245db98477f51bf5822f7676b639ab184ea
João Pedro <jpedrodeamorim@gmail.com>
2023-05-02 22:16:46 -0300

    use the defined constant

--- src/modules/mux2x1.hpp
+++ src/modules/mux2x1.hpp
@@ -1,18 +1,18 @@
-#include "defines.h"
+#include "definitions.hpp"
 #include <systemc.h>
 
 SC_MODULE(mux2x1) {
     sc_in<bool> sel;
-    sc_in<sc_uint<32>> a, b;
-    sc_out<sc_uint<32>> out;
+    sc_in<WORD> a, b;
+    sc_out<WORD> out;
+
+    void select() {
+        if (sel.read()) out.write(a.read());
+        else out.write(b.read());
+    }
 
     SC_CTOR(mux2x1) {
         SC_METHOD(select);
         sensitive << sel << a << b;
     }
-
-    void select() {
-        if (sel.read()) out.write(a.read());
-        else out.write(b.read());
-    }   
 };
