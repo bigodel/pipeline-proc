8d18dd92933c71f87748f59ba88ca72e151605f8
João Pedro <jpedrodeamorim@gmail.com>
2023-05-03 12:15:23 -0300

    add sign_extender

---
+++ src/modules/sign_extender.hpp
@@ -0,0 +1,20 @@
+#include "systemc.h"
+#include "definitions.hpp"
+
+SC_MODULE(sign_extender) {
+    sc_in<sc_uint<16>> input;
+    sc_out<WORD> output;
+
+    void extend() {
+        // if the sign bit is set, extend with 1s
+        if (input.read()[15] == 1)
+            output.write(0b1111111100000000 | input.read());
+        else // otherwise, extend with 0s
+            output.write(input.read());
+    }
+
+    SC_CTOR(sign_extender) {
+        SC_METHOD(extend);
+        sensitive << input;
+    }
+};
