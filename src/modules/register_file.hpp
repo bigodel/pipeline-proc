ee8a51aa48b7c3ecaed3eb5bcd871a25a9334a0e
João Pedro <jpedrodeamorim@gmail.com>
2023-05-02 23:27:45 -0300

    changed names to make it clearer

--- src/modules/register_file.hpp
+++ src/modules/register_file.hpp
@@ -2,24 +2,24 @@
 #include "systemc.h"
 
 SC_MODULE(register_file) {
-    sc_in<ADDR> reg1, reg2, write_reg;
+    sc_in<ADDR> src1, src2, dest;
     sc_in<WORD> write_data;
-    sc_out<WORD> out_data1, out_data2;
+    sc_out<WORD> reg1, reg2;
     sc_in<bool> reg_write;
 
     WORD registers[REG_COUNT] = {0};
 
     void register_process() {
-        out_data1.write(registers[reg1.read()]);
-        out_data2.write(registers[reg2.read()]);
+        reg1.write(registers[src1.read()]);
+        reg2.write(registers[src2.read()]);
 
         if (reg_write.read() == true) {
-            registers[write_reg.read()] = write_data.read();
+            registers[dest.read()] = write_data.read();
         }
     }
 
     SC_CTOR(register_file) {
         SC_METHOD(register_process);
-        sensitive << reg1 << reg2 << write_reg << reg_write << write_data;
+        sensitive << src1 << src2 << dest << reg_write << write_data;
     }
 };
