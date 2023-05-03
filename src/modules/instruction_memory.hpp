c1cc00b97591e206429a4ac8c6e4751884cca42f
João Pedro <jpedrodeamorim@gmail.com>
2023-05-02 23:02:20 -0300

    instruction memory implementation

---
+++ src/modules/instruction_memory.hpp
@@ -0,0 +1,18 @@
+#include "definitions.hpp"
+#include <systemc.h>
+
+SC_MODULE(instruction_memory) {
+    sc_in<WORD> address;      // input address
+    sc_out<WORD> instruction; // output instruction
+
+    SC_CTOR(instruction_memory) {
+        SC_METHOD(read_instruction);
+        sensitive << address;
+        instruction.initialize(0);
+    }
+
+    WORD mem[256] = {0}; // initialize memory with zeroes
+
+    // need to divide it by 4 since we are using a 32 bit word (4 bytes)
+    void read_instruction() { instruction.write(mem[address.read() / 4]); }
+};
