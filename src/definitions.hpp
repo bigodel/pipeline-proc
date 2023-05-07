03882c0fc66ea78e2edc5f0cf5860d3ffb6d5ca2
João Pedro <jpedrodeamorim@gmail.com>
2023-05-07 13:35:22 -0300

    add macros for better readability

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -20,4 +20,21 @@
 #define NOT 0b101
 #define CMP 0b110
 
+// opcodes
+// r-type
+#define OP_AND 0b0000
+#define OP_OR  0b0001
+#define OP_XOR 0b0010
+#define OP_NOT 0b0011
+#define OP_CMP 0b0100
+#define OP_ADD 0b0101
+#define OP_SUB 0b0110
+// i-type
+#define OP_LD  0b0111
+#define OP_ST  0b1000
+// j-type
+#define OP_J   0b1001
+#define OP_JN  0b1010
+#define OP_JZ  0b1011
+
 #endif /* DEFINITIONS_H */
